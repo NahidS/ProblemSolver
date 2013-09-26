68df2bb3833f43ad8f44efb1931441e32e33ba3b
nana1 <nahid.seidi@gmail.com>
2013-09-27 01:12:02 +0200

    Initial Commit

---
+++ ProblemSolver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProblemSolver
+//
+//  Created by Nahid Seidi on 13/9/27.
+//  Copyright (c) 2013 Nahid Seidi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
